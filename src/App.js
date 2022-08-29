2a040de5dc6207dac9184749e41a9792fae38bca
Ömer Kor <60571310+omerKor@users.noreply.github.com>
2022-08-29 16:48:47 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import { useRoutes } from "react-router-dom";
+import {useRoutes} from "react-router-dom";
 import routes from "routes";
-import { Toaster } from "react-hot-toast";
-import { useSelector } from "react-redux";
+import {Toaster} from "react-hot-toast";
+import {useSelector} from "react-redux";
 import Loader from "components/Loader";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 
 function App() {
 
 	const user = useSelector(state => state.auth.user)
 	const showRoutes = useRoutes(routes)
-	const [redirect, setRedirect] = useState(false)
 
-	useEffect(() => {
-		let timeout = setTimeout(() => {
-			setRedirect(true)
-		}, 1000)
-		return () => {
-			clearTimeout(timeout)
-		}
-	}, []);
-
-
-	if (!user && !redirect) {
+	if (user === null) {
 		return <Loader />
 	}
 
